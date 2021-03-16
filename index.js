f1a23ce2b537855d927ad922f75c412032f2d3db
Taxa8888 <Taxa8888@bk.ru>
2021-03-16 11:31:18 +0300

    prototype theme

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("Hello")
+const person = new Object(
+    {
+        name: 'Pavel',
+        age: 31,
+        greet: function () {
+            console.log('Greet!');
+        }
+    }
+)
+
+Object.prototype.sayHello = function() {
+    console.log('Hello!');
+}
+
+const lena = Object.create(person)
+lena.name = 'Elena'
+
+const str = new String('I am string')
